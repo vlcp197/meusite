e2872b4685c06f561688f60623389aaecf649ef4
vlcp197 <vlcp197@gmail.com>
2022-06-03 10:05:31 -0300

    Carregando URLs dinamicamente

--- pagina/urls.py
+++ pagina/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('',views.index, name='index'),
+    path('',views.index, name='pagina_inicial'),
     path('sobre/', views.sobre, name='sobre')
 ]
